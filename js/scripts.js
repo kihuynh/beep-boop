057f43cbd93091d5ad48d2ffcf4196286eaa9a2c
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-01-19 15:28:36 -0800

    populate text into front-end html

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
-// 0 with beep
-// 1 with a boop
-// % 3 = Sorry daaaave
+//back-end logic //
 
-var userInput = "";
-var beepBeep = "";
-var boopBoop = "";
+// var userInput = "";
+// var beepBeep = "";
+// var boopBoop = "";
 
 function shit(number) {
   var numberResult = [];
   var beep = "Beep";
   var boop = "Boop";
-  var ahShit = "bugger off";
+  var ahShit = "I can't do that. Bugger off";
 
   for ( i = 0; i <= number; i++) {
     if (number === 0) {
       return ("beep");
     } else if (number === 1) {
-      return alert (boop)
+      return alert (boop);
     } else if (number % 3 === 0) {
-      return alert (ahShit)
-    }
-    return
+      return alert (ahShit);
+    } else
+    return alert ("Go away. I don't work like this.");
   }
 
 };
@@ -29,10 +27,8 @@ $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
 
-    var number = $("input#numberInput").val();
-    // var userArray = Array.from(userInput)
+    var number = parseInt($("input#numberInput").val());
     var result = shit(number);
     $("#outputResult").text(result);
-    shit();
   });
 });
