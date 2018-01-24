33f78efc34ff50309008c0ba902e7a2821e027aa
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-01-23 21:18:56 -0800

    redo for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,33 @@
-//back-end logic //
 
-// var userInput = "";
-// var beepBeep = "";
-// var boopBoop = "";
-
-function shit(number) {
-  var numberResult = [];
-  var beep = "Beep";
-  var boop = "Boop";
-  var beepieBoops = "That's not how I work.";
-  var ahShit = "Sorry Dave. You don't pay me enough to do this";
-
-  for ( i = 0; i <= number; i++) {
-    if (number === 0) {
-      return alert(boop);
-    } else if (number === 1) {
-      return alert (boop);
-    } else if (number % 3 === 0) {
-      return alert (ahShit);
-    } else if (number != 1 && number != 0) {
-      return alert (beepieBoops)
-    } else
-    return alert ("Go away. I can't work in these conditions");
+function numberInput(number){
+// var numberInput = function(number) {
+  var numberResult= [];
+  for (var i = 0; i <= number; i++) {
+    // console.log(numberResult);
+    numberResult= [];
+//numbers that contain 0 will output "beep"
+    if (i=== 0) {
+      numberResult.push("beep");
+    } else if (i === 1) {
+      numberResult.push("boop");
+    } else if (i % 3 === 0) {
+      numberResult.push("I'm sorry, Dave")
+    }
+    else {
+      numberResult.push(i);
+    }
   }
-
+  return numberResult;
 };
-
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
 
-
-    var number = parseInt($("input#numberInput").val());
-    var result = shit(number);
+    var userNumber = parseInt($("input#numberInput").val());
+    var result = numberInput(userNumber);
 
     $("form#input").trigger('reset');
     $("#outputResult").text(result);
+    console.log(result);
   });
 });
